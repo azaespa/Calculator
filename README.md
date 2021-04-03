1fe3e1bda1179ce82605858220b2bcc5c79beca5
letsdothispej <pauljamesazanes@gmail.com>
2021-04-04 01:07:45 +0800

    Functions added

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 PATCH 1.0 
 Two text fields for numbers
 One button for computation
+
+PATCH 1.1
+Added 3 buttons for numbers 1-3
+and 2 buttons for operators + and -
+it can function like an actual calculator now
+except it only has 3 numbers available.
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const calculator = {
-    plus: function(a, b) {
-        return `The sum of ${a} and ${b} is equals to ${a + b}`;
+    plus: function (a, b) {
+        return a + b;
     },
     sub: function (a, b) {
-        return `The difference of ${a} and ${b} is equals to ${a - b}`;
+        return a - b;
     },
     mul: function (a, b) {
         return `The product of ${a} and ${b} is equals to ${a * b}`;
@@ -11,14 +11,14 @@ const calculator = {
     div: function (a, b) {
         return `The quotient of ${a} and ${b} is equals to ${a / b}`;
     },
-    showAll: function(a, b) {
+    showAll: function (a, b) {
         const allAnswers = [
-            calculator.plus(a,b),
-            calculator.sub(a,b),
-            calculator.mul(a,b),
-            calculator.div(a,b)
+            calculator.plus(a, b),
+            calculator.sub(a, b),
+            calculator.mul(a, b),
+            calculator.div(a, b)
         ]
-        return allAnswers.toString().replaceAll(",", ". "); 
+        return allAnswers.toString().replaceAll(",", ". ");
     }
 }
 
@@ -28,7 +28,7 @@ const submitButton = document.querySelector("#submitButton");
 
 let FIRST_NUMBER, SECOND_NUMBER;
 
-function getFirstNumber(a){
+function getFirstNumber(a) {
     FIRST_NUMBER = Number(a.target.value);
 }
 
@@ -36,7 +36,7 @@ function getSecondNumber(a) {
     SECOND_NUMBER = Number(a.target.value);
 }
 
-function showNumber(){
+function showNumber() {
     const answer = document.createElement("p");
     answer.appendChild(document.createTextNode(calculator.showAll(FIRST_NUMBER, SECOND_NUMBER)));
     document.body.appendChild(answer);
@@ -45,3 +45,56 @@ function showNumber(){
 firstNumber.addEventListener("input", getFirstNumber);
 secondNumber.addEventListener("input", getSecondNumber);
 submitButton.addEventListener("click", showNumber);
+
+const BUTTON_ONE = document.querySelector('#one');
+const BUTTON_TWO = document.querySelector('#two');
+const BUTTON_THREE = document.querySelector('#three');
+const BUTTON_ADDITION = document.querySelector('#addition');
+const BUTTON_SUBTRACTION = document.querySelector('#subtraction');
+
+let INPUT_NUMBER, STORE_INPUT_NUMBER, OPERATOR;
+let OPERATOR_MODE = false;
+
+function SET_NUMBER(ANY_BUTTON_NUMBER_CLICKED) {
+    INPUT_NUMBER = ANY_BUTTON_NUMBER_CLICKED.target.innerHTML;
+    if (OPERATOR_MODE) {
+        firstNumber.value = INPUT_NUMBER;
+        OPERATOR_MODE = false;
+    } else {
+        firstNumber.value += INPUT_NUMBER;
+    }
+}
+
+function SET_OPERATOR(ANY_BUTTON_OPERATOR_CLICKED) {
+    OPERATOR = ANY_BUTTON_OPERATOR_CLICKED.target.innerHTML;
+    let TOTAL = firstNumber.value;
+    
+    if (OPERATOR_MODE) {
+        secondNumber.value = secondNumber.value.toString().slice(0, secondNumber.value.toString().length - 1) + OPERATOR;
+    } else {
+        if (secondNumber.value != '') {
+            TOTAL = FIND_OPERATOR(secondNumber.value, firstNumber.value);
+        }
+        secondNumber.value = TOTAL + OPERATOR;
+        OPERATOR_MODE = true;
+    }
+}
+
+function FIND_OPERATOR(b, a) {
+    const FIRST_NUMBER_TO_INTEGER = Number(a);
+    const SECOND_NUMBER_TO_STRING = b.toString();
+    const SECOND_NUMBER_TO_INTEGER = Number(SECOND_NUMBER_TO_STRING.slice(0, SECOND_NUMBER_TO_STRING.length - 1))
+
+    switch (SECOND_NUMBER_TO_STRING.slice(-1)) {
+        case '+':
+            return calculator.plus(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+        case '-':
+            return calculator.sub(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+    }
+}
+
+BUTTON_ONE.addEventListener("click", SET_NUMBER);
+BUTTON_TWO.addEventListener("click", SET_NUMBER);
+BUTTON_THREE.addEventListener("click", SET_NUMBER);
+BUTTON_ADDITION.addEventListener("click", SET_OPERATOR);
+BUTTON_SUBTRACTION.addEventListener("click", SET_OPERATOR);
