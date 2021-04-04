39e1ecf652819dafd2e873ee574a010c1caba23b
letsdothispej <pauljamesazanes@gmail.com>
2021-04-04 12:34:52 +0800

    added decimal and equal buttons

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Added 3 buttons for numbers 1-3
 and 2 buttons for operators + and -
 it can function like an actual calculator now
 except it only has 3 numbers available.
+
+PATCH 1.2
+Added 7 buttons for numbers 4-9 and zero
+and 2 buttons for operators * and /
+still can function like an actual calculator
+but now has a complete digit needed and 
+basic operators 
+style has been added to arrange the buttons properly
+
+PATCH 1.3
+Added 1 button for decimal.
+Added 1 button for equals 
+Still can function like an actual calculator
+except it doesnt clear the text fields yet and giving an error after clicking 
+the number buttons again after clicking the equal button.
--- script.js
+++ script.js
@@ -56,19 +56,23 @@ const BUTTON_SEVEN = document.querySelector('#seven');
 const BUTTON_EIGHT = document.querySelector('#eight');
 const BUTTON_NINE = document.querySelector('#nine');
 const BUTTON_ZERO = document.querySelector('#zero');
+const BUTTON_DECIMAL = document.querySelector('#decimal');
 const BUTTON_ADDITION = document.querySelector('#addition');
 const BUTTON_SUBTRACTION = document.querySelector('#subtraction');
 const BUTTON_MULTIPLICATION = document.querySelector('#multiplication');
-const BUTTON_DIVITION = document.querySelector('#divition');
+const BUTTON_DIVISION = document.querySelector('#division');
+const BUTTON_EQUALS = document.querySelector('#equals');
 
 let INPUT_NUMBER, STORE_INPUT_NUMBER, OPERATOR;
 let OPERATOR_MODE = false;
+let GRAND_TOTAL_MODE = false;
 
 function SET_NUMBER(ANY_BUTTON_NUMBER_CLICKED) {
     INPUT_NUMBER = ANY_BUTTON_NUMBER_CLICKED.target.innerHTML;
     if (OPERATOR_MODE) {
         firstNumber.value = INPUT_NUMBER;
         OPERATOR_MODE = false;
+        GRAND_TOTAL_MODE = false;
     } else {
         firstNumber.value += INPUT_NUMBER;
     }
@@ -77,7 +81,6 @@ function SET_NUMBER(ANY_BUTTON_NUMBER_CLICKED) {
 function SET_OPERATOR(ANY_BUTTON_OPERATOR_CLICKED) {
     OPERATOR = ANY_BUTTON_OPERATOR_CLICKED.target.innerHTML;
     let TOTAL = firstNumber.value;
-
     if (OPERATOR_MODE) {
         secondNumber.value = secondNumber.value.toString().slice(0, secondNumber.value.toString().length - 1) + OPERATOR;
     } else {
@@ -85,6 +88,7 @@ function SET_OPERATOR(ANY_BUTTON_OPERATOR_CLICKED) {
             TOTAL = FIND_OPERATOR(secondNumber.value, firstNumber.value);
         }
         secondNumber.value = TOTAL + OPERATOR;
+        firstNumber.value = TOTAL;
         OPERATOR_MODE = true;
     }
 }
@@ -92,9 +96,10 @@ function SET_OPERATOR(ANY_BUTTON_OPERATOR_CLICKED) {
 function FIND_OPERATOR(b, a) {
     const FIRST_NUMBER_TO_INTEGER = Number(a);
     const SECOND_NUMBER_TO_STRING = b.toString();
+    const OPERATOR = SECOND_NUMBER_TO_STRING.slice(-1);
     const SECOND_NUMBER_TO_INTEGER = Number(SECOND_NUMBER_TO_STRING.slice(0, SECOND_NUMBER_TO_STRING.length - 1))
 
-    switch (SECOND_NUMBER_TO_STRING.slice(-1)) {
+    switch (OPERATOR) {
         case '+':
             return calculator.plus(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
         case '-':
@@ -104,6 +109,34 @@ function FIND_OPERATOR(b, a) {
         case '/':
             return calculator.div(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
     }
+    console.log("Grand total works!");
+}
+
+function SET_GRAND_TOTAL () {
+    GRAND_TOTAL_MODE = true;
+    const OPERATOR = secondNumber.value.toString().slice(-1);
+    const FIRST_NUMBER_TO_INTEGER = Number(firstNumber.value);
+    const SECOND_NUMBER_TO_STRING = secondNumber.value.toString().slice(0, secondNumber.value.toString().length - 1);
+    const SECOND_NUMBER_TO_INTEGER = Number(SECOND_NUMBER_TO_STRING);
+    let GRAND_TOTAL;
+
+    switch (OPERATOR) {
+        case '+':
+            GRAND_TOTAL = calculator.plus(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+            break;
+        case '-':
+            GRAND_TOTAL = calculator.sub(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+            break;
+        case '*':
+            GRAND_TOTAL = calculator.mul(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+            break;
+        case '/':
+            GRAND_TOTAL = calculator.div(SECOND_NUMBER_TO_INTEGER, FIRST_NUMBER_TO_INTEGER);
+            break;
+    }
+
+    secondNumber.value += firstNumber.value;
+    firstNumber.value = Number(GRAND_TOTAL);
 }
 
 BUTTON_ONE.addEventListener("click", SET_NUMBER);
@@ -116,7 +149,9 @@ BUTTON_SEVEN.addEventListener("click", SET_NUMBER);
 BUTTON_EIGHT.addEventListener("click", SET_NUMBER);
 BUTTON_NINE.addEventListener("click", SET_NUMBER);
 BUTTON_ZERO.addEventListener("click", SET_NUMBER);
+BUTTON_DECIMAL.addEventListener("click", SET_NUMBER);
 BUTTON_ADDITION.addEventListener("click", SET_OPERATOR);
 BUTTON_SUBTRACTION.addEventListener("click", SET_OPERATOR);
 BUTTON_MULTIPLICATION.addEventListener("click", SET_OPERATOR);
-BUTTON_DIVITION.addEventListener("click", SET_OPERATOR);
+BUTTON_DIVISION.addEventListener("click", SET_OPERATOR);
+BUTTON_EQUALS.addEventListener("click", SET_GRAND_TOTAL);
